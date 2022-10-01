88e22bac1bc3afa75ba378a8004bc518049a68cb
honey the codewitch <gaztoast@gmail.com>
2022-10-01 02:29:32 -0700

    cleanup

--- EspMon.cs
+++ EspMon.cs
@@ -11,10 +11,6 @@ namespace EspMon
         float cpuUsage;
         float gpuTemp;
         float gpuUsage;
-        float cpuPowerDrawPackage;
-        float cpuFrequency;
-        float gpuCoreFrequency;
-        float gpuMemoryFrequency;
         SerialPort _port;
         Computer _computer = new Computer()
 		{
@@ -89,87 +85,32 @@ namespace EspMon
             {
                 if (hardware.HardwareType == HardwareType.CPU)
                 {
-                    // only fire the update when found
                     hardware.Update();
-
-                    // loop through the data
                     foreach (var sensor in hardware.Sensors)
                         if (sensor.SensorType == SensorType.Temperature && sensor.Name.Contains("CPU Package"))
                         {
-                            // store
                             cpuTemp = sensor.Value.GetValueOrDefault();
-                            // print to console
-                            //System.Diagnostics.Debug.WriteLine("cpuTemp: " + sensor.Value.GetValueOrDefault());
-
                         }
                         else if (sensor.SensorType == SensorType.Load && sensor.Name.Contains("CPU Total"))
                         {
-                            // store
                             cpuUsage = sensor.Value.GetValueOrDefault();
-                            // print to console
-                            //System.Diagnostics.Debug.WriteLine("cpuUsage: " + sensor.Value.GetValueOrDefault());
-
-                        }
-                        else if (sensor.SensorType == SensorType.Power && sensor.Name.Contains("CPU Package"))
-                        {
-                            // store
-                            cpuPowerDrawPackage = sensor.Value.GetValueOrDefault();
-                            // print to console
-                           // System.Diagnostics.Debug.WriteLine("CPU Power Draw - Package: " + sensor.Value.GetValueOrDefault());
-
-
-                        }
-                        else if (sensor.SensorType == SensorType.Clock && sensor.Name.Contains("CPU Core #1"))
-                        {
-                            // store
-                            cpuFrequency = sensor.Value.GetValueOrDefault();
-                            // print to console
-                           // System.Diagnostics.Debug.WriteLine("cpuFrequency: " + sensor.Value.GetValueOrDefault());
                         }
                 }
-
 
                 // Targets AMD & Nvidia GPUS
                 if (hardware.HardwareType == HardwareType.GpuAti || hardware.HardwareType == HardwareType.GpuNvidia)
                 {
-                    // only fire the update when found
                     hardware.Update();
-
-                    // loop through the data
                     foreach (var sensor in hardware.Sensors)
                         if (sensor.SensorType == SensorType.Temperature && sensor.Name.Contains("GPU Core"))
                         {
-                            // store
                             gpuTemp = sensor.Value.GetValueOrDefault();
-                            // print to console
-                            //System.Diagnostics.Debug.WriteLine("gpuTemp: " + sensor.Value.GetValueOrDefault());
                         }
                         else if (sensor.SensorType == SensorType.Load && sensor.Name.Contains("GPU Core"))
                         {
-                            // store
                             gpuUsage = sensor.Value.GetValueOrDefault();
-                            // print to console
-                            //System.Diagnostics.Debug.WriteLine("gpuUsage: " + sensor.Value.GetValueOrDefault());
                         }
-                        else if (sensor.SensorType == SensorType.Clock && sensor.Name.Contains("GPU Core"))
-                        {
-                            // store
-                            gpuCoreFrequency = sensor.Value.GetValueOrDefault();
-                            // print to console
-                           // System.Diagnostics.Debug.WriteLine("gpuCoreFrequency: " + sensor.Value.GetValueOrDefault());
-                        }
-                        else if (sensor.SensorType == SensorType.Clock && sensor.Name.Contains("GPU Memory"))
-                        {
-                            // store
-                            gpuMemoryFrequency = sensor.Value.GetValueOrDefault();
-                            // print to console
-                           // System.Diagnostics.Debug.WriteLine("gpuMemoryFrequency: " + sensor.Value.GetValueOrDefault());
-                        }
-
                 }
-
-                // ... you can access any other system information you want here
-
             }
         }
 
