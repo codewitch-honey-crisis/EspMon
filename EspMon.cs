7b703701e1a6f9e3e51eb7364490e8cb57a2904f
honey the codewitch <gaztoast@gmail.com>
2022-10-03 08:11:20 -0700

    updated code to use container

--- EspMon.cs
+++ EspMon.cs
@@ -3,15 +3,67 @@ using System.Text;
 using System.Windows.Forms;
 using System.IO.Ports;
 using OpenHardwareMonitor.Hardware;
+using System.Collections.Generic;
+
 namespace EspMon
 {
-	public partial class EspMon : Form
+    public enum PartType
+    {
+        Processor,
+        GraphicsCard,
+        Memory
+    }
+    public enum MetricType
+    {
+        Temperature,
+        UsagePercentage,
+        PowerDrawWatts,
+        FrequencyHz,
+        VideoMemoryFrequencyHz
+    }
+    public class Container
+    {
+        public Dictionary<PartType, Dictionary<MetricType, float>> Data = new Dictionary<PartType, Dictionary<MetricType, float>>();
+
+        public Container()
+        {
+            foreach (var part in (PartType[])Enum.GetValues(typeof(PartType)))
+            {
+                Data.Add(part, GetMetricsForPart(part));
+            }
+        }
+
+        private static Dictionary<MetricType, float> GetMetricsForPart(PartType part)
+        {
+            var result = new Dictionary<MetricType, float>
+            {
+                { MetricType.Temperature, 0 },
+                { MetricType.UsagePercentage, 0 },
+                { MetricType.FrequencyHz, 0 }
+            };
+
+            switch (part)
+            {
+                case PartType.Processor:
+                    result.Add(MetricType.PowerDrawWatts, 0);
+                    break;
+                case PartType.GraphicsCard:
+                    result.Add(MetricType.PowerDrawWatts, 0);
+                    result.Add(MetricType.VideoMemoryFrequencyHz, 0);
+                    break;
+            }
+
+            return result;
+        }
+    }
+    public partial class EspMon : Form
 	{
         float cpuTemp;
         float cpuUsage;
         float gpuTemp;
         float gpuUsage;
         SerialPort _port;
+        Container _container = new Container();
         Computer _computer = new Computer()
 		{
 			CPUEnabled = true,
@@ -87,41 +139,74 @@ namespace EspMon
             {
                 if (hardware.HardwareType == HardwareType.CPU)
                 {
+                    // only fire the update when found
                     hardware.Update();
+
+                    var metrics = _container.Data[PartType.Processor];
+                    // loop through the data
                     foreach (var sensor in hardware.Sensors)
-                        if (sensor.SensorType == SensorType.Temperature &&
-                            sensor.Name.Contains("CPU Package"))
+                    {
+                        var value = sensor.Value.GetValueOrDefault();
+                        switch (sensor.SensorType)
                         {
-                            cpuTemp = sensor.Value.GetValueOrDefault();
+                            case SensorType.Temperature:
+                                metrics[MetricType.Temperature] = value;
+                                break;
+                            case SensorType.Load:
+                                metrics[MetricType.UsagePercentage] = value;
+                                break;
+                            case SensorType.Clock:
+                                metrics[MetricType.FrequencyHz] = value;
+                                break;
+                            case SensorType.Power:
+                                metrics[MetricType.PowerDrawWatts] = value;
+                                break;
                         }
-                        else if (sensor.SensorType == SensorType.Load && 
-                            sensor.Name.Contains("CPU Total"))
-                        {
-                            cpuUsage = sensor.Value.GetValueOrDefault();
-                        }
+                    }
                 }
 
+
                 // Targets AMD & Nvidia GPUS
-                if (hardware.HardwareType == HardwareType.GpuAti ||
-                    hardware.HardwareType == HardwareType.GpuNvidia)
+                if (hardware.HardwareType == HardwareType.GpuAti || hardware.HardwareType == HardwareType.GpuNvidia)
                 {
+                    // only fire the update when found
                     hardware.Update();
+
+                    var metrics = _container.Data[PartType.GraphicsCard];
+                    // loop through the data
                     foreach (var sensor in hardware.Sensors)
-                        if (sensor.SensorType == SensorType.Temperature && 
-                            sensor.Name.Contains("GPU Core"))
+                    {
+                        var value = sensor.Value.GetValueOrDefault();
+                        switch (sensor.SensorType)
                         {
-                            gpuTemp = sensor.Value.GetValueOrDefault();
+                            case SensorType.Temperature:
+                                metrics[MetricType.Temperature] = value;
+                                break;
+                            case SensorType.Load:
+                                metrics[MetricType.UsagePercentage] = value;
+                                break;
+                            case SensorType.Clock:
+                                if (sensor.Name.Contains("Core"))
+                                {
+                                    metrics[MetricType.FrequencyHz] = value;
+                                }
+                                else if (sensor.Name.Contains("Memory"))
+                                {
+                                    metrics[MetricType.VideoMemoryFrequencyHz] = value;
+                                }
+                                break;
+                            case SensorType.Power:
+                                metrics[MetricType.PowerDrawWatts] = value;
+                                break;
                         }
-                        else if (sensor.SensorType == SensorType.Load && 
-                            sensor.Name.Contains("GPU Core"))
-                        {
-                            gpuUsage = sensor.Value.GetValueOrDefault();
-                        }
+                    }
                 }
+
+                // ... you can access any other system information you want here
             }
         }
 
-		private void PortCombo_SelectedIndexChanged(object sender, EventArgs e)
+        private void PortCombo_SelectedIndexChanged(object sender, EventArgs e)
 		{
             if(_port!=null && _port.IsOpen)
 			{
@@ -156,25 +241,25 @@ namespace EspMon
                     _port.Read(cha, 0, cha.Length);
                     if ((char)cha[0] == '#')
                     {
-                        var ba = BitConverter.GetBytes(cpuUsage);
+                        var ba = BitConverter.GetBytes(_container.Data[PartType.Processor][MetricType.UsagePercentage]);
                         if(!BitConverter.IsLittleEndian)
 						{
                             Array.Reverse(ba);
 						}
                         _port.Write(ba, 0, ba.Length);
-                        ba = BitConverter.GetBytes(cpuTemp);
+                        ba = BitConverter.GetBytes(_container.Data[PartType.Processor][MetricType.Temperature]);
                         if (!BitConverter.IsLittleEndian)
                         {
                             Array.Reverse(ba);
                         }
                         _port.Write(ba, 0, ba.Length);
-                        ba = BitConverter.GetBytes(gpuUsage);
+                        ba = BitConverter.GetBytes(_container.Data[PartType.GraphicsCard][MetricType.UsagePercentage]);
                         if (!BitConverter.IsLittleEndian)
                         {
                             Array.Reverse(ba);
                         }
                         _port.Write(ba, 0, ba.Length);
-                        ba = BitConverter.GetBytes(gpuTemp);
+                        ba = BitConverter.GetBytes(_container.Data[PartType.GraphicsCard][MetricType.Temperature]);
                         if (!BitConverter.IsLittleEndian)
                         {
                             Array.Reverse(ba);
